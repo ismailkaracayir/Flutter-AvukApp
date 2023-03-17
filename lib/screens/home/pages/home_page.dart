089a5611502c55fe4c1be09c4f484d43b6115686
Utku Bilgin <utkubilgin47@gmail.com>
2023-03-17 22:41:15 +0300

    ana sayfa
    ana sayfa için card yapılarına başlandı

--- lib/screens/home/pages/home_page.dart
+++ lib/screens/home/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:avukapp/screens/home/constant/my_constant.dart';
 import 'package:flutter/material.dart';
 
+import '../../../widgets/main_cart.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -11,58 +13,105 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Padding(
-        padding: const PagePadding.allNormal(),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Aramak istediğiniz kelimeyi yaz...",
-                labelText: "Ara",
-                prefixIcon: const Padding(
-                  padding: EdgeInsets.only(left: 12.0, right: 6),
-                  child: Icon(
-                    Icons.search,
-                    color: kMyBackgroundColor,
+        padding: const PagePadding.symtcHrztNormal(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Aramak istediğiniz kelimeyi yaz...",
+                  labelText: "Ara",
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.only(left: 12.0, right: 6),
+                    child: Icon(
+                      Icons.search,
+                      color: kMyBackgroundColor,
+                    ),
+                  ),
+                  hintStyle: const TextStyle(color: kMyBackgroundColor),
+                  labelStyle: const TextStyle(color: kMyBackgroundColor),
+                  // floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(
+                      color: kMyPrimaryTextColor,
+                      width: 1.5,
+                    ),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(
+                      color: kMyBackgroundColor,
+                      width: 1.5,
+                    ),
+                    gapPadding: 10,
                   ),
                 ),
-                hintStyle: const TextStyle(color: kMyBackgroundColor),
-                labelStyle: const TextStyle(color: kMyBackgroundColor),
-                // floatingLabelBehavior: FloatingLabelBehavior.always,
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 16,
-                ),
-                enabledBorder: OutlineInputBorder(
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: _width * 0.65,
+                height: _height * 0.15,
+                decoration: BoxDecoration(
+                  color: Colors.green.shade200,
                   borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(
-                    width: 2,
-                    color: kMyPrimaryColor,
-                  ),
-                  gapPadding: 10,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: const BorderSide(
-                    color: kMyPrimaryTextColor,
-                    width: 1.5,
-                  ),
-                  gapPadding: 10,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return const Text("Utku");
-                },
-              ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              Container(
+                height: 450,
+                width: _width,
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.8,
+                  ),
+                  itemBuilder: (context, index) {
+                    return MainCart(
+                      ad: Text("Utku"),
+                      alan: Text("Bilgin"),
+                      deneyim: Text("Bilgisayar Mühendisi"),
+                      profilUrl: "",
+                      randevuButtom: Container(),
+                      soruButton: Container(),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+
+/*
+
+Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Container(
+                          width: _width * 0.4,
+                          height: 300,
+                          color: Colors.green,
+                          child: const Text("data"),
+                        ),
+                      ),
+                    );
+
+ */
